20dc19826331198bf38808e47b0b2b189c0c7f3d
John Morsley <johnmorsley@gmail.com>
2018-09-21 19:47:08 +0100

    Add project files.

---
+++ LearningCQRSwithMediatR/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LearningCQRSwithMediatR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
