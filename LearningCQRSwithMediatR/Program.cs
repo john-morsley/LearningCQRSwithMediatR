f90b0f8fa30f94dfbc6846d2d0210152da4dd165
John Morsley <johnmorsley@gmail.com>
2018-09-22 11:34:13 +0100

    Tidied.

--- LearningCQRSwithMediatR/Program.cs
+++ LearningCQRSwithMediatR/Program.cs
@@ -2,11 +2,15 @@
 
 namespace LearningCQRSwithMediatR
 {
+
     class Program
     {
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
+
     }
+
 }
